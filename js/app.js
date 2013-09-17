3494aee2ea55cf9b353149e106a9ae3903ae69fd
Chad Nelson <chadbnelson@gmail.com>
2013-09-16 21:35:01 -0400

    Fixing typos. @michelledimeo++

--- js/app.js
+++ js/app.js
@@ -5,9 +5,10 @@
 angular.module('glamHack', []).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/home.html'});
-    $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'MyCtrl2'});
-    $routeProvider.when('/what', {templateUrl: 'partials/what.html', controller: 'MyCtrl2'});
-    $routeProvider.when('/who', {templateUrl: 'partials/who.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/about', {templateUrl: 'partials/about.html'});
+    $routeProvider.when('/what', {templateUrl: 'partials/what.html'});
+    $routeProvider.when('/where&when', {templateUrl: 'partials/wherewhen.html'});
+    $routeProvider.when('/who', {templateUrl: 'partials/who.html'});
     $routeProvider.when('/challenges', {templateUrl: 'partials/challenges.html'});
     $routeProvider.when('/data', {templateUrl: 'partials/data.html'});
     $routeProvider.when('/coc', {templateUrl: 'partials/coc.html'});
